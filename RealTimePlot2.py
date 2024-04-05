35cb454c74eaf7a1fd53a33bbdcf1197a11fdbe4
jorgecard <121685867+jorgecard@users.noreply.github.com>
2024-04-05 14:26:38 -0500

    6 panels

--- RealTimePlot2.py
+++ RealTimePlot2.py
@@ -5,92 +5,170 @@ import matplotlib.animation as animation
 from tkinter import *
 from threading import Thread
 import sys
-import struct
-from pymodbus.client.sync import ModbusTcpClient
+
+import struct # Para decodificar los datos
+from pymodbus.client.sync import ModbusTcpClient # Para la conexión
+
 import time
 
 # Realizar la conexión
-ip = '192.168.0.76'
+ip = '192.168.0.76' # Coloca la dirección IP de tu multímetro
 client = ModbusTcpClient(ip, port=502, timeout=3)
 client.connect()
 
-# Definir diccionarios para almacenar los datos
-data_dict_1 = {
-    'Voltage': {'offset': 1, 'reg': 2, 'color': 'b-', 'label': 'Voltaje', 'unit': ' [V]'},
-    'Voltage2': {'offset': 3, 'reg': 2, 'color': '#ADD8E6', 'label': 'Voltaje2', 'unit': ' [V]'}
+# Definir un diccionario para almacenar los datos
+data_dict = {
+  'Tensión L1-N': {'offset': 1, 'reg': 2, 'color': '#9103A6', 'label': 'Tensión L1-N', 'unit': '[V]', 'graphic':1, 'factor':1},
+  'Tensión L2-N': {'offset': 3, 'reg': 2, 'color': '#9103A6', 'label': 'Tension L2-N', 'unit': '[V]','graphic':1, 'factor':1},
+  'Tensión L3-N': {'offset': 5, 'reg': 2, 'color': '#9103A6', 'label': 'Tensión L3-N', 'unit': '[V]','graphic':1,'factor':1},
+  'Tensión L1-L2': {'offset': 7, 'reg': 2, 'color': '#A6036A', 'label': 'Tensión L1-L2', 'unit': '[V]','graphic':1,'factor':1},
+  'Tensión L2-L3': {'offset': 9, 'reg': 2, 'color': '#A6036A', 'label': 'Tensión L2-L3', 'unit': '[V]','graphic':1,'factor':1},
+  'Tensión L3-L1': {'offset': 11, 'reg': 2, 'color': '#A6036A', 'label': 'Tensión L3-L1', 'unit': '[V]','graphic':1,'factor':1},
+  'I-L1': {'offset': 13, 'reg': 2, 'color': '#DF8905', 'label': 'I-L1', 'unit': '[A]','graphic':2,'factor':1},
+  'I-L2': {'offset': 15, 'reg': 2, 'color': '#DF8905', 'label': 'I-L2', 'unit': '[A]','graphic':2,'factor':1},
+  'I-L3': {'offset': 17, 'reg': 2, 'color': '#DF8905', 'label': 'I-L3', 'unit': '[A]','graphic':2,'factor':1},
+  'E-S': {'offset': 63, 'reg': 2, 'color': '#54548D', 'label': 'E-S', 'unit': '[VA]','graphic':3,'factor':1},
+  'E-P': {'offset': 65, 'reg': 2, 'color': '#558D54', 'label': 'E-P', 'unit': '[W]','graphic':3,'factor':1},
+  'E-Q': {'offset': 63, 'reg': 2, 'color': '#547E8D', 'label': 'E-Q', 'unit': '[VAR]','graphic':3,'factor':1},
+  'THD-V1': {'offset': 261, 'reg': 2, 'color': '#ADD8E6', 'label': 'THD-V1', 'unit': '[%]','graphic':4,'factor':1},
+  'THD-V2': {'offset': 263, 'reg': 2, 'color': '#ADD8E6', 'label': 'THD-V2', 'unit': '[%]','graphic':4,'factor':1},
+  'THD-V3': {'offset': 265, 'reg': 2, 'color': '#ADD8E6', 'label': 'THD-V3', 'unit': '[%]','graphic':4,'factor':1},
+  'THD-I1': {'offset': 267, 'reg': 2, 'color': '#EF2110', 'label': 'THD-I1', 'unit': '[%]','graphic':4,'factor':1},
+  'THD-I2': {'offset': 269, 'reg': 2, 'color': '#EF2110', 'label': 'THD-I2', 'unit': '[%]','graphic':4,'factor':1},
+  'THD-I3': {'offset': 271, 'reg': 2, 'color': '#EF2110', 'label': 'THD-I3', 'unit': '[%]','graphic':4,'factor':1},
+  'Energía Aparente': {'offset': 2817, 'reg': 2, 'color': '#A74CEE', 'label': 'Energía Aparente', 'unit': '[kVAh]','graphic':5,'factor':1}
 }
 
-data_dict_2 = {
-    'Current': {'offset': 13, 'reg': 2, 'color': 'r-', 'label': 'Corriente', 'unit': ' [A]'},
-    'Current2': {'offset': 15, 'reg': 2, 'color': 'r-', 'label': 'Corriente2', 'unit': ' [A]'}
-}
-
-def getData(data_dict):
-    time.sleep(1)
-    while isRun:
-        global isReceiving
-        global value
-        for key, val in data_dict.items():
-            raw_value = client.read_holding_registers(address=val['offset'], count=val['reg'], unit=1)
-            packed_value = struct.pack('>I',(raw_value.registers[0]<<16)|raw_value.registers[1])
-            value[key] = round(struct.unpack('!f', packed_value)[0], 2)
-        isReceiving = True
+def getData():
+  time.sleep(1)
+  while (isRun):
+    global isReceiving
+    global value
+    for key, val in data_dict.items():
+      raw_value = client.read_holding_registers(address=val['offset'], count=val['reg'], unit = 1)
+      packed_value = struct.pack('>I',(raw_value.registers[0]<<16)|raw_value.registers[1])
+      value[key] = round(struct.unpack('!f', packed_value)[0], 2)
+    isReceiving = True
 
 def onClosing():
-    global isRun
-    isRun = False
-    thread.join()
-    root.quit()
-    root.destroy()
+  global isRun
+  isRun = False
+  thread.join()
+  root.quit()
+  root.destroy()
 
-def plotData(self, lines, lineValueText, data_dict):
-    for key, val in data_dict.items():
-        data[key].append(value.get(key, 0))  # Obtener el valor o 0 si la clave no está presente
-        lines[key].set_data(range(Samples), data[key])
-        lineValueText[key].set_text(val['label'] + ' = ' + str(value.get(key, 0)) + val['unit'])
+def plotData(self, lines, lineValueText, lineLabel):
+  for key, val in data_dict.items():
+    data[key].append(value[key])
+    lines[key].set_data(range(Samples), data[key])
+    lineValueText[key].set_text(val['label'] +' = '+ str(value[key]) + val['unit'])
 
 # Plot Parameters -----------------
 Samples = 100
-sampleTime = 100  # Tiempo de muestreo / Sample Time
-value = {key: 0 for key in data_dict_1.keys()}  # Inicializar todas las claves con el valor 0
-data = {key: collections.deque([0] * Samples, maxlen=Samples) for key in data_dict_1.keys()}
+sampleTime = 100  #Tiempo de muestreo / Sample Time
+value = {}
+data = {}
 isReceiving = False
 isRun = True
 
-# Crear graficas -------------------------------------------------------
-fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 6), facecolor='0.94')
+for key, val in data_dict.items():
+  data[key] = collections.deque([0]*Samples, maxlen=Samples)
+  value[key] = 0
 
-axes = [ax1, ax2]
-data_dicts = [data_dict_1, data_dict_2]
+# Limites de los ejes / Axis limit
+xmin = 0
+xmax = Samples
 
-for ax, data_dict in zip(axes, data_dicts):
-    ax.grid()
-    ax.set_xlabel('Muestras')
-    ax.set_ylabel('Magnitud')
+# Crear gráficas -------------------------------------------------------
+fig, ((ax1, ax2, ax3), (ax4, ax5, _)) = plt.subplots(2, 3, figsize=(12, 6), facecolor='0.94')
 
-    lines = {}
-    lineValueText = {}
+# Primera subtrama
+ax1.set_title('Monitoreo en tiempo real - Izquierda Superior')
+ax1.grid()
+ax1.set_xlabel('Muestras')
+ax1.set_ylabel('Magnitud')
+ax1.set_xlim(xmin, xmax)
+ax1.set_ylim(0, 300)
 
-    for i, (key, val) in enumerate(data_dict.items()):
-        lines[key], = ax.plot([], [], val['color'], label=val['label'])
-        lineValueText[key] = ax.text(0.65, 0.90 - i * 0.05, '', transform=ax.transAxes)
+# Segunda subtrama
+ax2.set_title('Monitoreo en tiempo real - Centro Superior')
+ax2.grid()
+ax2.set_xlabel('Muestras')
+ax2.set_ylabel('Magnitud')
+ax2.set_xlim(xmin, xmax)
+ax2.set_ylim(0, 5)
 
-# Iniciar hilo para obtener los datos
-thread = Thread(target=getData, args=(data_dict_1,))
+# Tercera subtrama
+ax3.set_title('Monitoreo en tiempo real - Derecha Superior')
+ax3.grid()
+ax3.set_xlabel('Muestras')
+ax3.set_ylabel('Magnitud')
+ax3.set_xlim(xmin, xmax)
+ax3.set_ylim(0, 800)
+
+# Cuarta subtrama
+ax4.set_title('Monitoreo en tiempo real - Izquierda Inferior')
+ax4.grid()
+ax4.set_xlabel('Muestras')
+ax4.set_ylabel('Magnitud')
+ax4.set_xlim(xmin, xmax)
+ax4.set_ylim(0, 5)
+
+# Quinta subtrama
+ax5.set_title('Monitoreo en tiempo real - Izquierda Inferior')
+ax5.grid()
+ax5.set_xlabel('Muestras')
+ax5.set_ylabel('Magnitud')
+ax5.set_xlim(xmin, xmax)
+ax5.set_ylim(0, 6)
+
+lines = {}
+lineValueText = {}
+
+count1 = 0
+count2 = 0
+count3 = 0
+count4 = 0
+count5 = 0
+for i, (key, val) in enumerate(data_dict.items()):
+    if val['graphic'] == 1:
+        ax = ax1
+        count1 += 1
+        count = count1
+    elif val['graphic'] == 2:
+        ax = ax2
+        count2 += 1
+        count = count2
+    elif val['graphic'] == 3:
+        ax = ax3
+        count3 += 1
+        count = count3
+    elif val['graphic'] == 4:
+        ax = ax4
+        count4 += 1
+        count = count4
+    elif val['graphic'] == 5:
+        ax = ax5
+        count5 += 1
+        count = count5
+    lines[key], = ax.plot([], [], val['color'], label=val['label'])
+    # lineValueText[key] = ax.text(0.65, 0.95 - count * 0.05, '', transform=ax.transAxes)
+    lineValueText[key] = ax.text(0.30, 0.95 - count * 0.05, '', transform=ax.transAxes)
+
+########## Recibir datos en segundo plano ##################
+thread = Thread(target=getData)
 thread.start()
 
-# Esperar hasta que se reciban los datos
-while not isReceiving:
-    print('Iniciando recepcion de datos')
-    time.sleep(0.1)
+while isReceiving != True:
+  print('iniciando recepcion de datos')
+  time.sleep(0.1)
 
-# Interfaz grafica
+################## Interfaz grafica ##################
 root = Tk()
 root.protocol('WM_DELETE_WINDOW', onClosing)
 canvas = FigureCanvasTkAgg(fig, master=root)
 canvas._tkcanvas.grid(row=0, column=0)
 
-# Animate
-anim = animation.FuncAnimation(fig, plotData, fargs=(lines, lineValueText, data_dict_1), interval=sampleTime)
+anim = animation.FuncAnimation(fig, plotData, fargs=(lines, lineValueText, data_dict), interval=sampleTime)
 
 root.mainloop()
